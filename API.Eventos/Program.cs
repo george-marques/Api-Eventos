92ec2fe7a970858d02c3b6682ce46ce3fbc76eb8
Jorginho <georgedanielmarques@gmail.com>
2024-12-03 10:31:21 -0300

    feat-Configuração para personalização da documentação

--- API.Eventos/Program.cs
+++ API.Eventos/Program.cs
@@ -1,7 +1,9 @@
 using API.Eventos.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Configuration;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Customizando nossas informações
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "API de Eventos",
+            Description = "Sistema de Gerenciamento de Eventos e Inscrições",
+            Contact = new OpenApiContact
+            {
+                Name = "Desenvolvido por George",
+                Url = new Uri("https://localhost:7141/swagger/index.html")
+            }
+        });
+
+    // Pegamos o nome no nosso arquivo Assembly e armazenamos na variável
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    // Pega o diretório base da aplicação e junta com o nome do arquivo que acabou de criar
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    c.IncludeXmlComments(xmlPath);
+});
+
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext"))
