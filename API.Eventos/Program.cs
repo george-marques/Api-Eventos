8d5f059782b5c7317ff35eaf442f7bed36c206a1
Jorginho <georgedanielmarques@gmail.com>
2024-12-03 14:46:32 -0300

    remove-Arquivos iniciais automaticos do projeto

--- API.Eventos/Program.cs
+++ API.Eventos/Program.cs
@@ -9,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 // Customizando nossas informações
 
