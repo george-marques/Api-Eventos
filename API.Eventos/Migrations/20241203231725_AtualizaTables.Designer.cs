d50cf9f9f6871389a11703dd71a179dbcb366d12
Jorginho <georgedanielmarques@gmail.com>
2024-12-03 20:20:19 -0300

    fix-Corrige migration fks

--- API.Eventos/Migrations/20241203231725_AtualizaTables.Designer.cs
+++ API.Eventos/Migrations/20241203231725_AtualizaTables.Designer.cs
@@ -4,6 +4,7 @@ using API.Eventos.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Eventos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241203231725_AtualizaTables")]
+    partial class AtualizaTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API.Eventos/Migrations/20241203231725_AtualizaTables.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Eventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizaTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Eventos_Locais_LocalId",
+                table: "Eventos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Eventos_Organizadores_OrganizadorId",
+                table: "Eventos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inscricoes_Participantes_ParticipanteId",
+                table: "Inscricoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inscricoes_ParticipanteId",
+                table: "Inscricoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Eventos_LocalId",
+                table: "Eventos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Eventos_OrganizadorId",
+                table: "Eventos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricoes_ParticipanteId",
+                table: "Inscricoes",
+                column: "ParticipanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_LocalId",
+                table: "Eventos",
+                column: "LocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_OrganizadorId",
+                table: "Eventos",
+                column: "OrganizadorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Eventos_Locais_LocalId",
+                table: "Eventos",
+                column: "LocalId",
+                principalTable: "Locais",
+                principalColumn: "LocalId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Eventos_Organizadores_OrganizadorId",
+                table: "Eventos",
+                column: "OrganizadorId",
+                principalTable: "Organizadores",
+                principalColumn: "OrganizadorId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inscricoes_Participantes_ParticipanteId",
+                table: "Inscricoes",
+                column: "ParticipanteId",
+                principalTable: "Participantes",
+                principalColumn: "ParticipanteId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
